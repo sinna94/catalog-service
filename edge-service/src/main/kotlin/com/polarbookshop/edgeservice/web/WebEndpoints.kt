bb9ad52882aa1a400d79ccf9c17298e1664762cc
chunghyun lee <sinna94@naver.com>
2024-05-07 21:43:55 +0900

    스프링 클라우드 게이트, 레디스를 통한 요청 사용률 제한

---
+++ edge-service/src/main/kotlin/com/polarbookshop/edgeservice/web/WebEndpoints.kt
@@ -0,0 +1,23 @@
+package com.polarbookshop.edgeservice.web
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+@Configuration
+class WebEndpoints {
+
+    @Bean
+    fun routerFunction(): RouterFunction<ServerResponse> {
+        return RouterFunctions.route()
+            .GET("/catalog-fallback") {
+                ServerResponse.ok().body(Mono.just(""), String::class.java)
+            }.POST("catalog-fallback") {
+                ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+            }.build()
+    }
+}
